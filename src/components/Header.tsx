e6c2e88f37bd2f08f8c770e99d5774f4962b6c2c
Travis Vignon <travis@lookitsatravis.com>
2020-12-14 14:24:15 -0600

    Update image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="header-container">
       <h1 className="header header-back">Dozen White Elephant</h1>
       <h1 className="header">Dozen White Elephant</h1>
-      <img src="/2020.png" className="header-img" />
+      <img src="/2020.png" className="header-img" alt="2020 Sucks" />
     </div>
   );
 }
